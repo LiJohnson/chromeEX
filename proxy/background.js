7321e9a5fa1dd2fb26f46e9e29fd79b83238c648
LiJohnson <598420668@163.com>
2014-11-26 12:14:14 +0800

    update

--- proxy/background.js
+++ proxy/background.js
@@ -14,23 +14,33 @@ var config = {
 		proxyForHttp: {
 			scheme: "http",
 			host: "192.168.199.111",
-			port:3128
+			port: 3128
 		},
 		bypassList: []
 	}
 };
-config = {"mode":"fixed_servers","rules":{"singleProxy":{"scheme":"http","host":"192.168.199.111","port":3128},"bypassList":[]}};
+config = {
+	"mode": "fixed_servers",
+	"rules": {
+		"singleProxy": {
+			"scheme": "http",
+			"host": "192.168.199.111",
+			"port": 3128
+		},
+		"bypassList": []
+	}
+};
 chrome.proxy.settings.set({
 		value: config,
 		scope: 'regular'
 	},
 	function(e) {
-		console.log("proxy" , e);
-});
-chrome.proxy.onProxyError.addListener(function (e){
+		console.log("proxy", e);
+	});
+chrome.proxy.onProxyError.addListener(function(e) {
 	console.log("proxy error");
 });
 
-document.addEventListener("DOMContentLoaded",function(e){
-	console.log("DOM loaded",e);
+document.addEventListener("DOMContentLoaded", function(e) {
+	console.log("DOM loaded", e);
 })
