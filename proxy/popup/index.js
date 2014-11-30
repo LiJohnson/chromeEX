32b569bdc903c8f30f32cda047e90779c7f770a7
LiJohnson <598420668@163.com>
2014-11-30 16:06:16 +0800

    添加地图(google map)连接

--- proxy/popup/index.js
+++ proxy/popup/index.js
@@ -1,7 +1,13 @@
 $(function(){
 	var p = new MyProxy();
+	var googleMap = "https://www.google.com.hk/maps/search/"
 	p.getIpInfo(function(data){
 		$(".city").html([data.city,data.region,data.country].join(" . "));
 		$(".flag").addClass("flag-icon flag-icon-"+data.country.toLowerCase());
+		$(".map").prop("href" ,  googleMap + data.loc );
+	});
+
+	$(".map").click(function() {
+		chrome.tabs.create({url:this.href,selected:false});
 	});
 });
