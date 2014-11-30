be41b9f643eef9170d9c6a18a59aa36a2c55bb74
LiJohnson <598420668@163.com>
2014-11-30 17:34:57 +0800

    优化功能

--- proxy/popup/index.js
+++ proxy/popup/index.js
@@ -2,9 +2,13 @@ $(function(){
 	var p = new MyProxy();
 	var googleMap = "https://www.google.com.hk/maps/search/"
 	p.getIpInfo(function(data){
+		var map = googleMap + data.loc;
+		$(".ip").html(data.ip);
 		$(".city").html([data.city,data.region,data.country].join(" . "));
 		$(".flag").addClass("flag-icon flag-icon-"+data.country.toLowerCase());
-		$(".map").prop("href" ,  googleMap + data.loc );
+		$(".map").removeClass('hide').prop("href" ,  map );
+		$("iframe").prop('src', map);
+		$(".loading").addClass('hide');
 	});
 
 	$(".map").click(function() {
